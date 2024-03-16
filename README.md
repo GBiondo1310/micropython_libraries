ae550116a97c9bd5ba1e3a54f31cf5cf952bede6
GBiondo1310 <giacomo.biondelli@gmail.com>
2024-03-16 11:09:32 +0100

    Initial commit - sab 16 mar 2024, 11:09:32, CET

---
+++ README.md
@@ -0,0 +1,11 @@
+# Micropython libraries to help with IDEs autocompletion.
+
+# Install in editable mode:
+```
+pip install -e .
+```
+
+# Install from github:
+```
+pip install project_name@git+"https://github.com/GBiondo1310/micropython_libraries.git#egg=<project_name>"
+```
---
+++ src/udnspkt.pyi
@@ -0,0 +1,18 @@
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+
+Node = Any
+
+def write_fqdn(buf: Any, name: str) -> None:
+    pass
+
+def skip_fqdn(buf: Any) -> None:
+    pass
+
+def make_req(buf: Any, fqdn: Any, is_ipv6: Any) -> None:
+    pass
+
+def parse_resp(buf: Any, is_ipv6: Any) -> Any:
+    pass
+
+#   0: return rval
+# ? 0: return rval
